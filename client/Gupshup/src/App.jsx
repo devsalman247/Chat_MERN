ae42611847b9374a4020affc98a78a65a178ed79
devsalman247 <salman247.dev@gmail.com>
2022-10-01 11:25:47 +0500

    project init

--- client/Gupshup/src/App.jsx
+++ client/Gupshup/src/App.jsx
@@ -1,41 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faComment } from "@fortawesome/free-regular-svg-icons";
 import { Icon } from '@iconify/react';
-import './App.css'
-
-// library.add(faComment);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {/* <FontAwesomeIcon icon="user" /> */}
-      {/* <FontAwesomeIcon icon="comment" /> */}
-      <Icon icon="bi:chat-left-dots" />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='flex items-start'>
+      Simple Page
     </div>
   )
 }
