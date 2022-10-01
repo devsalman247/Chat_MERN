7426391c2cadba93cc21e5e2b469bdc44a4b8c7b
devsalman247 <salman247.dev@gmail.com>
2022-10-01 09:31:20 +0500

    project init

--- client/Gupshup/src/App.jsx
+++ client/Gupshup/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { Icon } from '@iconify/react';
 import './App.css'
+
+// library.add(faComment);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +30,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      {/* <FontAwesomeIcon icon="user" /> */}
+      {/* <FontAwesomeIcon icon="comment" /> */}
+      <Icon icon="bi:chat-left-dots" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
