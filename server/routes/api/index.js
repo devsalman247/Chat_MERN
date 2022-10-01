943f905d6de8726758bc1d673782966dcebab583
devsalman247 <salman247.dev@gmail.com>
2022-10-01 19:29:38 +0500

    updated routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/user', require('./user'));
 router.use('/friend', require('./friend'));
+router.use('/group', require('./group'));
 router.use('/chat', require('./chat'));
 
 module.exports = router;
