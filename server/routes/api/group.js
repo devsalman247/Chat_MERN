c93e5eeeb732cd804529e550432619c4d9d7aa2a
devsalman247 <salman247.dev@gmail.com>
2022-10-02 00:05:25 +0500

    fixes

--- server/routes/api/group.js
+++ server/routes/api/group.js
@@ -1,4 +1,9 @@
-const router = require('express').Router();
+const router = require('express').Router(), 
+      Group = require('../../models/Group');
 
+router.post('/create', (req, res, next) => {
+    const group = new Group();
+    console.log('Group created');
+})
 
 module.exports = router;
