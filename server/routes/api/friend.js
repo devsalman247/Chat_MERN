f8d410985071100c421099e07d3deca9b87f2798
devsalman247 <salman247.dev@gmail.com>
2022-10-03 20:23:41 +0500

    added new endpoint

--- server/routes/api/friend.js
+++ server/routes/api/friend.js
@@ -54,4 +54,48 @@ router.post("/add", (req, res, next) => {
   }
 });
 
+router.post("/new", (req, res, next) => {
+  if(!req.body.id) {
+    next(new BadRequestResponse("Please provide user id to send request"));
+  }else {
+    User.findById(req.user.id, (error, myProfile) => {
+      if (error) {
+        next(
+          new BadRequestResponse(`Request couldn't be processed..Try again!`)
+        );
+      } else if (!myProfile) {
+        next(new BadRequestResponse("Profile not found!"));
+      } else if (myProfile) {
+        const index = myProfile.requests.findIndex(
+          (user) => user.requestId == req.body.id
+        );
+        if (index === -1) {
+          next(new BadRequestResponse('User not found'));
+        } else {
+          User.findById(req.body.id, (error, user) => {
+            if(error) {
+              next(new BadRequestResponse(error.message));
+            }else if(user) {
+              const userIndex = user.requests.findIndex(
+                (user) => user.requestId === req.user.id
+              );
+              if (index === -1) {
+                next(new BadRequestResponse('User not found'));
+              } else {
+                myProfile.requests.splice(index,1);
+                user.requests.splice(userIndex,1);
+                myProfile.friends.push(req.body.id);
+                user.friends.push(req.user.id);
+                myProfile.save();
+                user.save();
+                res.send(`You're now friends`);
+              }
+            }
+          })
+        }
+      }
+    })
+  }
+})
+
 module.exports = router;
