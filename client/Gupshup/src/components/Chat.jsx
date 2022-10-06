7152ddce40e5d0ba0950dbdcc59896e060aa713c
devsalman247 <salman247.dev@gmail.com>
2022-10-06 22:26:42 +0500

    some fixes

--- client/Gupshup/src/components/Chat.jsx
+++ client/Gupshup/src/components/Chat.jsx
@@ -1,19 +1,36 @@
-import { useState,useEffect } from "react"
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Chat() {
+  const [friends, setFriends] = useState([]);
   const token = localStorage.getItem("chatToken");
+
   useEffect(() => {
-    axios.get("http://localhost:3000/api/friend/all", {
-      headers: { Authorization: `Token ${token}` },
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-  },[]);
-    // const 
+    axios
+      .get("http://localhost:3000/api/friend/all", {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => setFriends(res.data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>Chat</div>
-  )
+    <>
+      <h2></h2>
+      <div>
+        {friends.length === 0
+          ? `You've no friends yet! Make new to chat`
+          : friends.map((user, key) => {
+              return (
+                <div key={key}>
+                  Name : {user.name}
+                  About : {user.about}
+                </div>
+              );
+            })}
+      </div>
+    </>
+  );
 }
 
-export default Chat
+export default Chat;
